da753894ba51e7bed9946dddc526ebfe52cdf74b
Brandon Colangelo <brandoncancode@gmail.com>
2023-05-30 14:02:44 -0400

    Fixed some logic

--- server.py
+++ server.py
@@ -10,10 +10,11 @@ app.secret_key = "Loki"
 # Root route.
 @app.route('/')
 def index():
-    if 'gold' and 'activity_log' and 'total_moves' not in session:
+    if 'gold' and 'activity_log' and 'total_moves' and 'gold_won' not in session:
         session['gold'] = 0
         session['activity_log'] = []
         session['total_moves'] = 0
+        session['gold_won'] = 0
     return render_template("index.html")
 
 # Route for processing gold and number of moves.
@@ -21,36 +22,30 @@ def index():
 def process_gold():
     # logic for winning the game based on moves and score (needs work)
     moves = 0
-    if session['gold'] < 500 and session['total_moves'] <= 15:
-        pass
+    if session['gold'] > 200 and session['total_moves'] <=15:
+        session['activity_log'].append("Congratulations, you've won!")
 
-    elif session['gold'] >= 500 and session['total_moves'] <= 15:
-        session['activity_log'].append(f"Congratulations, you've won!")
-
-    elif session['gold'] < 500 and session['total_moves'] > 15:
-        session['activity_log'].append(f"Game Over")
-    else:
-        session['activity_log'].append(f"Game Over")
-
+    # Adds gold earned and number of moves to session
     if 'farm' == request.form['property'] or 'cave' == request.form['property'] or 'house' == request.form['property']:
-        gold_won = random.randint(10,20)
+        session['gold_won'] = random.randint(10,20)
         moves += 1
-        session['gold'] += gold_won
+        session['gold'] += session['gold_won']
         session['total_moves'] += moves
-        session['activity_log'].append(f"You earned {gold_won} gold nuggets!")
+        session['activity_log'].append(f"You earned {session['gold_won']} gold nuggets!")
         return redirect('/')
 
+    # Adds gold earned/lost and number of moves to session
     if 'casino' == request.form['property']:
-        gold_won = random.randint(-50,50)
+        session['gold_won'] = random.randint(-50,50)
         moves += 1
-        if gold_won < 0:
-            session['gold'] += gold_won
+        if session['gold_won'] < 0:
+            session['gold'] += session['gold_won']
             session['total_moves'] += moves
-            session['activity_log'].append(f"You lost {gold_won} gold nuggets!")
+            session['activity_log'].append(f"You lost {session['gold_won']} gold nuggets!")
         else:
-            session['gold'] += gold_won
+            session['gold'] += session['gold_won']
             session['total_moves'] += moves
-            session['activity_log'].append(f"You earned {gold_won} gold nuggets!")
+            session['activity_log'].append(f"You earned {session['gold_won']} gold nuggets!")
         return redirect('/')
 
 
@@ -64,6 +59,6 @@ def reset():
 if __name__=='__main__':
     app.run(debug=True, host='localhost', port=5001)
 
-# change the color of the activity log based on positive or negative gold earned
-# limit the number of moves to get a certain score
+# change the color of the activity log based on positive or negative gold earned(Done)
 # list the activity log in descending order(Done)
+# limit the number of moves and set the score that has to be reached to win the game
